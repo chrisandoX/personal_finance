8509997fda773e93662ee9ec62a3bc817e44dd40
Chris <zurad92@gmail.com>
2019-11-02 13:12:25 +0100

    Iterate over transaction list

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    BNPTransactionParser *BNPParser = new BNPTransactionParser;
+
+    QFile BNP("/Users/chris/tutorial-1/revolute.csv");
+    if(BNP.open(QIODevice::ReadOnly))
+    {
+        QList<Transaction> transcations = BNPParser->parseTransactions(&BNP);
+
+        QList<Transaction>::iterator i;
+        for (i = transcations.begin(); i != transcations.end(); ++i)
+        {
+            qDebug() << i->date;
+            qDebug() << i->reference;
+            qDebug() << i->category;
+            qDebug() << i->amount;
+        }
+
+        BNP.close();
+    }
+    else
+    {
+        qDebug() << "File not found";
+    }
 }
 
 MainWindow::~MainWindow()
