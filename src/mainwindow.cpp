c038d47da4a3a1a2336ef18f65839158e759c550
Chris <zurad92@gmail.com>
2019-11-03 13:17:20 +0100

    Refactor handling files

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -23,37 +23,34 @@ MainWindow::MainWindow(QWidget *parent)
     RevoluteTransactionParser *RevoluteParser = new RevoluteTransactionParser;
 
     QFile Revolute("/Users/chris/personal_finance_app/personal_finanace/revolute.csv");
-    if(Revolute.open(QIODevice::ReadOnly))
-    {
-
-        QList<Transaction> transcations;
-        try
-        {
-            transcations = RevoluteParser->parseTransactionList(&Revolute);
-        }
-        catch (const char* exception) // catch exceptions of type const char*
-        {
-           qDebug() << "Error: " << exception;
-        }
-
-        QList<Transaction>::iterator i;
-        for (i = transcations.begin(); i != transcations.end(); ++i)
-        {
-            if(db.isOpen())
-            {
-                db.addTransaction(*i);
-            }
-            else
-            {
-                qDebug() << "Database is not open!";
-            }
-        }
-
-        Revolute.close();
-    }
-    else
+    if(!Revolute.open(QIODevice::ReadOnly))
     {
         qDebug() << "File not found";
+        return;
+    }
+
+    QList<Transaction> transcations;
+    try
+    {
+        transcations = RevoluteParser->parseTransactionList(&Revolute);
+        Revolute.close();
+    }
+    catch (const char* exception) // catch exceptions of type const char*
+    {
+       qDebug() << "Error: " << exception;
+    }
+
+    QList<Transaction>::iterator i;
+    for (i = transcations.begin(); i != transcations.end(); ++i)
+    {
+        if(db.isOpen())
+        {
+            db.addTransaction(*i);
+        }
+        else
+        {
+            qDebug() << "Database is not open!";
+        }
     }
 }
 
